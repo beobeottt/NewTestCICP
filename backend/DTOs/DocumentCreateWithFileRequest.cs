8d20718fdc366e1e5f8c496f3f81e80304f98f12
long <baolongp9@gmail.com>
2026-04-03 13:17:02 +0700

    s

--- backend/DTOs/DocumentCreateWithFileRequest.cs
+++ backend/DTOs/DocumentCreateWithFileRequest.cs
@@ -13,12 +13,11 @@ public class DocumentCreateWithFileRequest
 
 public class UpdateDocumentReq
 {
-    public string title { get; set; } = string.Empty;
-    public string description { get; set; } = string.Empty;
-    public string content { get; set; } = string.Empty;
-    public DocumentRole Role { get; set; } = DocumentRole.Pending;
-    public string CreateBy { get; set; } = string.Empty;
-
+    public string? title { get; set; }
+    public string? description { get; set; }
+    public string? content { get; set; }
+    public DocumentRole? Role { get; set; }
+    public string? CreateBy { get; set; }
     public IFormFile? File { get; set; }
 }
 
